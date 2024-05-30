3ebffd8e96d17981c2cdf3b22d3b943970ee8f28
Sarthak Jain <sarthakj.study.cs@gmail.com>
2024-05-31 00:00:01 +0530

    added aws s3

--- src/components/ProfileInfoForm.tsx
+++ src/components/ProfileInfoForm.tsx
@@ -1,17 +1,30 @@
 'use client';
 import { saveProfile } from "@/actions/ProfileInfoActions";
+import { uploadToS3 } from "@/actions/uploadActions";
+import { ChangeEvent } from "react";
 
 export default function ProfileInfoForm() {
     async function handleFormAction(formData: FormData) {
         const result = await saveProfile(formData);
-        console.log(result);
-
+        console.log(result)
     }
+    async function upload(ev:ChangeEvent<HTMLInputElement>) {
+        const target = ev.target as HTMLInputElement;
+        if (target.files?.length) {
+            const file = target.files[0];
+            const formData = new FormData();
+            formData.append('file', file);
+            console.log(await uploadToS3(formData));
+    }
+}
     return(
         <form action={handleFormAction}>
             <div className="bg-gray-200 p-4 rounded-lg">
                 <div className="bg-gray-300 size-24 rounded-full p-4">avatar</div>
-                <div>cover image</div>
+                <div>
+                    cover image
+                    <input type="file" onChange={ev => upload(ev)} />
+                </div>
             </div>
             <div>
                 <label className="block mt-4" htmlFor="usernameIn">username</label>
