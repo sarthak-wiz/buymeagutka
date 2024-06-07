1abececfa330a33fed39374dce21b233895b456a
Sarthak Jain <sarthakj.study.cs@gmail.com>
2024-06-08 01:11:48 +0530

    Add Home page component with introductory content and call-to-action

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,29 @@
+import {faStar} from "@fortawesome/free-solid-svg-icons";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
 export default function Home() {
   return (
     <section className="max-w-lg mx-auto text-center mt-16">
-      <h1 className="text-6xl font-bold">
-        Fund your<br />
+      <div className="text-gray-600">
+        <p>
+          <FontAwesomeIcon icon={faStar}/>
+          <FontAwesomeIcon icon={faStar}/>
+          <FontAwesomeIcon icon={faStar}/>
+          <FontAwesomeIcon icon={faStar}/>
+          <FontAwesomeIcon icon={faStar}/>
+        </p>
+        <p className="mt-2">Loved by 100,000+ creators</p>
+      </div>
+      <h1 className="text-6xl font-bold mt-4">
+        Fund your<br/>
         creative work
       </h1>
       <h2 className="mt-4 mb-8">
-        Accept support for your work. <br />
+        Accept support for your work.<br/>
         It's easier than you think.
       </h2>
       <button className="bg-yellow-300 px-8 py-4 font-bold rounded-full">
-        start my page
+        Start my page
       </button>
     </section>
   );
